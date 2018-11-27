c374e1d46a5bbc93c15ad85d256a50e40c3ed626
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-11-27 03:21:22 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,13 +6,13 @@ def key_for_min_value(name_hash)
  if name_hash == {}
    return nil
  else 
-  smallest = name_hash.first
-
-  name_hash.each do |key, value|
-    if value < name_hash[smallest]
-      smallest = key
-    end
-  end
+   smallest = name_hash.first
+   name_hash.each do |key, value|
+     if value < name_hash[smallest]
+       smallest = key
+     end
+ end
+  
   smallest
   
  end
